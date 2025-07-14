29289c0b3c1fe65ac377246b298485df82cf6c85
Frank <128847456+flexxterio@users.noreply.github.com>
2025-07-14 23:52:16 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Bot is running!"
+def home():
+    return "Bot is running"
 
 def keep_alive():
     app.run(host="0.0.0.0", port=8080)
